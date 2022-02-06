7790cec1bd07713a9291d5b6a238d6264c74e9f5
Andy Liu <aliu927@gmail.com>
2022-02-05 16:05:50 -0800

    added a setInterval to keep connection alive

--- TelloCLI.js
+++ TelloCLI.js
@@ -1,41 +1,60 @@
-//Require Section
-const dgram = require('dgram');
-const readline = require('readline');
+//--Require Section
+const dgram = require("dgram");
+const readline = require("readline");
+const { clearInterval } = require("timers");
 
-//Send Command & Recieve Response
-const telloIp = '192.168.10.1';
+//--Send Command & Recieve Response
+const telloIp = "192.168.10.1";
 const telloPort = 8889;
 
-//Recieve Tello State ( For Data Telemetry Stuff )
-const udpServer = '0.0.0.0';
+//--Recieve Tello State ( For Data Telemetry Stuff)
+const udpServer = "0.0.0.0";
 const udpPort = 8890;
+const udpVideoPort = 11111;
 
-//Tx & Rx To Tello
-const djiTello = dgram.createSocket('udp4');
+//--Tx & Rx To Tello
+const djiTello = dgram.createSocket("udp4");
 
-//Event Emitters from Tello
-djiTello.on('message',(message,rinfo)=>{
-    console.log(`${message} from ${rinfo.address}:${rinfo.port}`);
+//--Event Emitters from Tello
+djiTello.on("message", (message, rinfo) => {
+  console.log(`${message} from ${rinfo.address}:${rinfo.port}`);
 });
 
-djiTello.on('error',(err)=>{
-    console.log(`Error:\n${err}`);
+djiTello.on("error", (err) => {
+  console.log(`Error:\n${err}`);
 });
 
-djiTello.on('listening',()=>{
-    const address = djiTello.address();
-    console.log(`Tello listening on ${address.address}:${address.port}`);
+djiTello.on("listening", () => {
+  const address = djiTello.address();
+  console.log(`Tello listening on ${address.address}:${address.port}`);
 });
 
 djiTello.bind(telloPort);
 
-//CLI To Tx To Tello
+//--CLI To Tx To Tello
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-    
-rl.on('line',(input)=>{
-    djiTello.send(input,0,input.length,telloPort,telloIp);
+  input: process.stdin,
+  output: process.stdout,
 });
 
+//--This Keeps the connection alive
+let countDown;
+
+function startInterval() {
+  countDown = setInterval(() => {
+    const dummy = "command";
+    djiTello.send(dummy, 0, dummy.length, telloPort, telloIp);
+  }, 15 * 1000);
+}
+
+function endInterval() {
+  clearInterval(countDown);
+}
+//--End Live Connnection
+
+//--This is the CLI that send commands to Tello
+rl.on("line", (input) => {
+  endInterval();
+  djiTello.send(input, 0, input.length, telloPort, telloIp);
+  startInterval();
+});
